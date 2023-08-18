6b7a7719634629eac17b3eabe9b4d85444cbd497
tyuXX <82142326+tyuXX@users.noreply.github.com>
2023-08-19 00:58:47 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+Random rng = new(new Random(new Random(new Random(new Random(new Random(new Random(new Random().Next()).Next()).Next()).Next()).Next()).Next()).Next());
+string pid = (rng.NextDouble() * rng.Next()).ToString();
+Console.Title = $"[{Assembly.GetExecutingAssembly().GetName().Name} v{Assembly.GetExecutingAssembly().GetName().Version!.ToString().Replace(".",string.Empty).TrimEnd('0')}] Pid:{pid}";
+
+while (true)
+{
+    
+}
